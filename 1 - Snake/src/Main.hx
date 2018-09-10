b54a21ce8da111a3be47a3be43ed07dcae36d506
EliteFun <33582077+EliteFun@users.noreply.github.com>
2018-09-09 20:29:11 -0400

    Remover resourcePath from Snake hxml

--- 1 - Snake/src/Main.hx
+++ 1 - Snake/src/Main.hx
@@ -1,11 +1,13 @@
 class Main extends hxd.App {
-    var bmp:h2d.Bitmap;
 
     override function init() {
-        new h2d.Bitmap(h2d.Tile.fromColor(0xFF0000, 60, 60), s2d);
+    }
+
+    override function update(dt) {
     }
 
     static function main() {
+        hxd.Res.initLocal();
         new Main();
     }
 }
